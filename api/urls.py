24d65d5ba9dc627d845f9e14d2e342bdf8ab7dd3
Jakub <jakubtolsciuk@gmail.com>
2022-05-01 19:49:05 +0000

    Add endpoints for 'club' model

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.getData),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('superendpoint/', views.superEndpoint)
+    path('participants/', views.ParticipantList.as_view()),
+    path('clubs/', views.ClubList.as_view()),
+    path('clubs/<int:pk>/', views.ClubDetail.as_view()),
+    # path('users/', views.UserList.as_view()),
+    # path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
-from api.models import User
+from api.models import User, Participant, Club
 from rest_framework.views import APIView
+from rest_framework import status
+from django.core import serializers
 
 #permission_classes = [permissions.AllowAny]
 
@@ -26,14 +28,50 @@ from rest_framework.views import APIView
 #def superEndpoint(request):
 #    return Response({'xd': 'xdddddd'})
 #
-class UserList(APIView):
+
+class ParticipantList(APIView):
+    """
+    List all Participants, or create a new Participant.
+    """
+    # * for testing allow any for all endopints in this class
+    permission_classes=[permissions.AllowAny]
+    # def get(self, request, format=None):
+    #     snippets = Snippet.objects.all()
+    #     serializer = SnippetSerializer(snippets, many=True)
+    #     return Response(serializer.data)
+
+    def post(self, request, format=None):
+        # TODO validation / serializers?
+        return Response(request.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class ParticipantDetail(APIView):
+
+#     def get(self, request, format=None):
+#         return Response({'test': 'test'})
+
+class ClubList(APIView):
+    """
+    List all Clubs, or create a new Club.
+    """
     permission_classes=[permissions.AllowAny]
 
-    #@permission_classes([permissions.IsAuthenticated])
     def get(self, request, format=None):
-        return Response({'test': 'test'})
+        clubs = serializers.serialize("json", Club.objects.all())
+        return Response(clubs)
 
-class UserDetail(APIView):
+    # permission_classes=[permissions.IsAuthenticated]
+
+    def post(self, request, format=None):
+        # TODO validation / serializers?
+        club = Club(club_name = request.data['club_name'], club_ceo = request.data['club_ceo'])
+        club.save()
+        return Response(request.data, status=status.HTTP_201_CREATED)
+
+class ClubDetail(APIView):
     permission_classes=[permissions.AllowAny]
+
     def get(self, request, pk, format=None):
-        return Response({'id': pk})
+        club = serializers.serialize("json", [Club.objects.get(id = pk)])
+        return Response(club)
+
